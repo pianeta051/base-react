cafed2aad18045fe44fbaf89f9f94e3753b5007f
Asun Gómez <asun.gomez@wazuh.com>
2022-07-17 11:25:09 +0200

    Mui (#1)
    * Install MUI
    
    * Install emotion
    
    * Add theming capabilities
    
    Co-authored-by: Maria.GomezCastro <Maria.GomezCastro@eu.jll.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { ThemeProvider } from "@mui/material";
 import { FC } from "react";
+import { theme } from "./theme/theme";
 
-export const App: FC = () => <>App</>;
+export const App: FC = () => <ThemeProvider theme={theme}></ThemeProvider>;
