92c1b9a5e42f8cd811f3169380a313bc4d749441
Maria.GomezCastro <Maria.GomezCastro@eu.jll.com>
2022-08-22 16:01:42 +0200

    Add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { ThemeProvider } from "@mui/material";
 import { FC } from "react";
+import { BrowserRouter } from "react-router-dom";
 import { theme } from "./theme/theme";
 
-export const App: FC = () => <ThemeProvider theme={theme}></ThemeProvider>;
+export const App: FC = () => (
+  <ThemeProvider theme={theme}>
+    <BrowserRouter></BrowserRouter>
+  </ThemeProvider>
+);
